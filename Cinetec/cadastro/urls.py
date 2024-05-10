11982d839cc16f01efb17d162cc1042367353a32
Vincenzo Bonatti <vincenzobonati@outlook.com>
2024-05-09 22:56:32 -0300

    pagina programação

--- Cinetec/cadastro/urls.py
+++ Cinetec/cadastro/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("cadastro_novo/", cadastroCreateView.as_view(), name="pagina-cadastro"),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='pagina-login'),
     path('logout/', auth_views.LogoutView.as_view(method = "POST"), name='pagina-logout'),
+    #path("logout/", auth_views.LoginView.as_view(template_name = 'logout.html'), name="pagina-logout"),
 ]
 
